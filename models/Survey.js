9335e41778325529a276af1b31d5cf752edd6aec
Aidan <84754533+aidan10371@users.noreply.github.com>
2021-07-23 15:08:47 -0400

    improved survey model

---
+++ models/Survey.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SurveySchema = new Schema({
+    title: {
+        type: String,
+        required: "A valid survey title is required",
+        match: [/([A-z0-9])+/g, "Special characters are not allowed"],
+    },
+    questions: [
+        {
+            name: {
+                type: String,
+                required: "A valid question name is required",
+                match: [/([A-z0-9])+/g, "Special characters are not allowed"],
+            },
+            type: {
+                type: String,
+                required: "A valid question type is required",
+                validate: [
+                    function(input) {
+                        let valid = true;
+                        if (input !== "text" && input !== "number" && input !== "rating") {
+                            valid = false;
+                        }
+                      return valid;
+                    },
+                    "Password should be longer."
+                  ]
+            }
+        }
+    ],
+    responses: []
+});
