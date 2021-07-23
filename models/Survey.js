16d8b8526c0d6c849f041dbd087fcd13152cd786
ka930 <77208047+ka930@users.noreply.github.com>
2021-07-23 15:23:03 -0400

    Add more features to Survey model

--- models/Survey.js
+++ models/Survey.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const ALLOWEDTYPES = ["text", "number", "rating"];
+
 const SurveyQuestion = new Schema({
-  question: { type: String, required: true },
-  responseType: { type: String, required: true },
+  question: { type: String, required: true, maxLength: 200 },
+  responseType: {
+    type: String,
+    required: "Response type required",
+    validate: {
+      validator: (type) => type in ALLOWEDTYPES,
+      message: "Valid types are: text, number, rating",
+    },
+  },
 });
 
 const SurveyResponse = new Schema({
@@ -16,6 +25,7 @@ const SurveySchema = new Schema({
   title: {
     type: String,
     required: true,
+    maxLength: 50,
   },
   questions: [SurveyQuestion],
   responses: [SurveyResponse],
