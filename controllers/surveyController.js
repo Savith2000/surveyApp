dfa7231501691091e6172b4951518d98f9e7d211
ka930 <77208047+ka930@users.noreply.github.com>
2021-07-26 14:46:06 -0400

    Add a response counter to the Survey model

--- controllers/surveyController.js
+++ controllers/surveyController.js
@@ -11,12 +11,19 @@ module.exports = {
       return db.Survey.create(survey);
     }),
 
-  addResponseToSurveyBySurveyId: (surveyId, response) =>
-    db.Survey.findById(surveyId).then((survey) =>
+  addResponseToSurveyBySurveyId: (surveyId, response) => {
+    incrementSurveyRespontCounter(surveyId);
+    return db.Survey.findById(surveyId).then((survey) =>
       this.addResponseToSurveyByResponseListId(survey.responses._id, response)
-    ),
+    );
+  },
+
+  // Do not use without calling incrementSurveyResponseCounter!
   addResponseToSurveyByResponseListId: (responseListId, response) =>
     db.ResponseList.findByIdAndUpdate(responseListId, {
       $push: { responses: response },
     }),
+
+  incrementSurveyResponseCounter: (surveyId) =>
+    db.survey.findByIdAndUpdate(surveyId, { $inc: { responseAmount: 1 } }),
 };
