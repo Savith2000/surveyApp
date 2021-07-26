bd293282896a9b9fab6f04b93dd0dd5370778007
Savith Tata <savithtata@gmail.com>
2021-07-26 10:00:23 -0400

    .

---
+++ models/Survey.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ALLOWEDTYPES = ["text", "number", "rating"];
+
+const SurveyQuestion = new Schema({
+  question: { type: String, required: true, maxLength: 200 },
+  responseType: {
+    type: String,
+    required: "Response type required",
+    validate: {
+      validator: (type) => type in ALLOWEDTYPES,
+      message: "Valid types are: text, number, rating",
+    },
+  },
+});
+
+const SurveyResponse = new Schema({
+  answers: [Schema.Type.Mixed],
+  date: { type: Date, required: true },
+});
+
+const SurveySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  creationDate: { type: Date, required: true },
+  questions: [SurveyQuestion],
+  responses: [SurveyResponse],
+});
+
+const SurveyModel = mongoose.model("Survey", SurveySchema);
+
+module.exports = SurveyModel;
